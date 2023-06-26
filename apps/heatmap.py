d556dabf63db4956e3ba6256a87d256a82a8c521
Mickey Lin <s.mickey@gmail.com>
2023-06-26 13:44:13 +0800

    Testing heatmap

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -8,13 +8,13 @@ def app():
 
     # filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/world_cities.csv"
     filepath = "https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/world_cities.csv"
-    m = leafmap.Map(tiles="stamentoner", center=(1.3521, 103.8198), zoom=100)
+    m = leafmap.Map(tiles="stamentoner", center=(1.3521, 103.8198), zoom=12)
     m.add_heatmap(
         filepath,
         latitude="latitude",
         longitude="longitude",
         value="pop_max",
         name="Heat map",
-        radius=20,
+        radius=100,
     )
     m.to_streamlit(height=700)
