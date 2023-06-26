b7b8470ff1c7b2edfda1ed1ac370ced542ad7272
Mickey Lin <s.mickey@gmail.com>
2023-06-26 11:40:05 +0800

    Testing buttons and page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ apps = [
     {"func": home.app, "title": "Connect AI", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
     {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
-    {"func": buttons.app, "title": "Buttons", "icon": "button"}
 ]
 
 titles = [app["title"] for app in apps]
