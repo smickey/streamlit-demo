ebb795af1e1be6d3f81668a3e81843692e0c8557
Mickey Lin <s.mickey@gmail.com>
2023-06-26 09:47:31 +0800

    First streamlit demo using template

--- apps/home.py
+++ apps/home.py
@@ -3,11 +3,11 @@ import leafmap.foliumap as leafmap
 
 
 def app():
-    st.title("Home")
+    st.title("Connect AI")
 
     st.markdown(
         """
-    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
+    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu).
     To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
     https://share.streamlit.io/giswqs/streamlit-template?page=upload
 
