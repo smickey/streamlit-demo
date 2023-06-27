45c980ecacd64b9d5bdb83af535ab5dc2c13dc5c
Mickey Lin <s.mickey@gmail.com>
2023-06-27 16:21:45 +0800

    Trying to fix cache

--- apps/home.py
+++ apps/home.py
@@ -5,7 +5,20 @@ import h3
 import branca.colormap as cm
 import requests
 
-@st.cache_session
+@st.cache(show_spinner=False)
+def get_hexagons(user_input):
+    response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
+    hex_data = {}
+    if response.status_code == 200:
+        hex_data_raw = response.json()
+        for key, value in hex_data_raw.items():
+            hexagon_id = list(value.keys())[0]
+            measurement = list(value.values())[0]
+            hex_data[hexagon_id] = measurement
+    else:
+        st.write("Error:", response.status_code)
+    return hex_data
+
 def app():
     st.title("Connect AI")
     st.markdown(
@@ -13,27 +26,12 @@ def app():
     Welcome to Connect AI! Where we connect you with your dates
     """
 )
-    # Create a text input box
     user_input = st.text_input("Where are you now?", "")
-    # Display the input text
     st.write("You are here at~", user_input)
 
-    # Call API
-    response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
-    if response.status_code == 200:
-        hex_data = response.json()
-    else:
-        st.write("Error:", response.status_code)
-    hexagons = {}
-    for key, value in hex_data.items():
-        hexagon_id = list(value.keys())[0]
-        measurement = list(value.values())[0]
-        hexagons[hexagon_id] = measurement
+    hexagons = get_hexagons(user_input)
 
-    # Plot map
     map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
-
-    # Add click event handler
     map.add_child(folium.LatLngPopup())
     color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
     for h3_index, time in hexagons.items():
@@ -41,12 +39,13 @@ def app():
         color = color_scale(time)
         folium.Polygon(
             locations=geo_boundary,
-        color=None,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.6,
-        popup=f'Time: {time:.2f} min'
+            color=None,
+            fill=True,
+            fill_color=color,
+            fill_opacity=0.6,
+            popup=f'Time: {time:.2f} min'
         ).add_to(map)
     st_folium(map, width=700, height=700)
+
 if __name__ == "__main__":
     app()
