a4d25074d0f4567cf5f6393c9c22306fe99abe35
Mickey Lin <s.mickey@gmail.com>
2023-06-27 16:56:35 +0800

    Trying to add a session state so doesn't update with every single move

--- apps/home.py
+++ apps/home.py
@@ -25,7 +25,7 @@ def app():
     """
     Welcome to Connect AI! Where we connect you with your dates
     """
-)
+    )
     if 'user_input' not in st.session_state:
         st.session_state['user_input'] = ''
         st.session_state['prev_user_input'] = ''
@@ -36,22 +36,22 @@ def app():
     if st.session_state['user_input'] and st.session_state['user_input'] != st.session_state['prev_user_input']:
         st.session_state['prev_user_input'] = st.session_state['user_input']
         hexagons = get_hexagons(st.session_state['user_input'])
-
-        map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
-        map.add_child(folium.LatLngPopup())
-        color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
-        for h3_index, time in hexagons.items():
-            geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
-            color = color_scale(time)
-            folium.Polygon(
-                locations=geo_boundary,
-                color=None,
-                fill=True,
-                fill_color=color,
-                fill_opacity=0.6,
-                popup=f'Time: {time:.2f} min'
-            ).add_to(map)
-        st_folium(map, width=700, height=700)
+        if hexagons:
+            map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
+            map.add_child(folium.LatLngPopup())
+            color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
+            for h3_index, time in hexagons.items():
+                geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
+                color = color_scale(time)
+                folium.Polygon(
+                    locations=geo_boundary,
+                    color=None,
+                    fill=True,
+                    fill_color=color,
+                    fill_opacity=0.6,
+                    popup=f'Time: {time:.2f} min'
+                ).add_to(map)
+            st_folium(map, width=700, height=700)
 
 if __name__ == "__main__":
     app()
