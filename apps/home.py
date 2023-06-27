1d097a580bf06d1b73aa1b3bf8d0e7744733da12
Mickey Lin <s.mickey@gmail.com>
2023-06-27 13:06:38 +0800

    Trying to add a legend

--- apps/home.py
+++ apps/home.py
@@ -5,29 +5,6 @@ import h3
 import branca.colormap as cm
 from branca.element import Template, MacroElement
 
-class ColorScaleLegend(MacroElement):
-    def __init__(self, color_scale, caption):
-        super(ColorScaleLegend, self).__init__()
-        self.color_scale = color_scale
-        self.caption = caption
-
-        self._template = Template("""
-            {% macro html(this, kwargs) %}
-            <div style="
-                position: fixed;
-                bottom: 50px;
-                left: 50px;
-                width: 200px;
-                height: 30px;
-                z-index:9999;
-                font-size:12px;
-                ">
-                <div style="background-color: {{this.color_scale}}; width: 100%; height: 100%;">&nbsp;</div>
-                <div>{{this.caption}}</div>
-            </div>
-            {% endmacro %}
-        """)
-
 def app():
     st.title("Connect AI")
     st.subheader("Finding you the best place to meet")
@@ -61,9 +38,6 @@ def app():
         fill_opacity=0.6,
         popup=f'Time: {time:.2f} min'
         ).add_to(map)
-
-    legend = ColorScaleLegend(color_scale, "Time in minutes")
-    map.get_root().add_child(legend)
 
     st_folium(map, width=1400, height=700)
 
