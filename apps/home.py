7d0cbeab99a54aab61911499e41fefe77dd8c8fe
Mickey Lin <s.mickey@gmail.com>
2023-06-27 13:28:46 +0800

    Testing new code

--- apps/home.py
+++ apps/home.py
@@ -3,14 +3,20 @@ import folium
 from streamlit_folium import st_folium
 import h3
 import branca.colormap as cm
-from branca.element import Template, MacroElement
 import requests
-
 st.title("Connect AI")
-st.subheader("Finding you the best place to meet")
-
+st.markdown(
+        """
+        Welcome to Connect AI! Where we connect you with your dates
+        """
+)
+# Create a text input box
+user_input = st.text_input("Where are you now?", "")
+# Display the input text
+st.write("You are here at~", user_input)
+@st.cache(allow_output_mutation=True)
 def app():
-     # Call API
+    # Call API
     response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
     if response.status_code == 200:
         hex_data = response.json()
@@ -21,10 +27,8 @@ def app():
         hexagon_id = list(value.keys())[0]
         measurement = list(value.values())[0]
         hexagons[hexagon_id] = measurement
-
     # Plot map
     map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
-
     # Add click event handler
     map.add_child(folium.LatLngPopup())
     color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
@@ -40,6 +44,5 @@ def app():
         popup=f'Time: {time:.2f} min'
         ).add_to(map)
     st_folium(map, width=1400, height=700)
-
 if __name__ == "__main__":
     app()
