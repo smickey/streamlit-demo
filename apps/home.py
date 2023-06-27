f6075181e7ec0be7d0050b996cd312ca07c13790
Mickey Lin <s.mickey@gmail.com>
2023-06-27 13:21:48 +0800

    Testing new code

--- apps/home.py
+++ apps/home.py
@@ -4,29 +4,31 @@ from streamlit_folium import st_folium
 import h3
 import branca.colormap as cm
 from branca.element import Template, MacroElement
+import requests
 
+@st.cache(allow_output_mutation=True)
 def app():
     st.title("Connect AI")
     st.subheader("Finding you the best place to meet")
-    # st.markdown()
 
-    # Base map
+     # Call API
+    response = requests.get(f"https://connectai-emwgdoqmma-de.a.run.app/traveltimeh3?locations={user_input}")
+    if response.status_code == 200:
+        hex_data = response.json()
+    else:
+        st.write("Error:", response.status_code)
+    hexagons = {}
+    for key, value in hex_data.items():
+        hexagon_id = list(value.keys())[0]
+        measurement = list(value.values())[0]
+        hexagons[hexagon_id] = measurement
+
+    # Plot map
     map = folium.Map(location=[1.3521, 103.8198], zoom_start=12, tiles="CartoDB Positron")
 
-    # Color scale for the different time
+    # Add click event handler
+    map.add_child(folium.LatLngPopup())
     color_scale = cm.LinearColormap(['green', 'yellow', 'red', 'purple'], vmin=0, vmax=120)
-    # color_scale.caption = 'Time in minutes'
-    # color_scale.add_to(map)
-
-    # Hexagon
-    hexagons = {
-        '8a652636062ffff': 0,
-        '8a6526360197fff': 11.600000000000001,
-        '8a652636051ffff': 13.700000000000001,
-        '8a6526360777fff': 3.7096248711548725,
-        '8a6526360767fff': 7.340706024792129,
-        # Add more hexagons here...
-    }
     for h3_index, time in hexagons.items():
         geo_boundary = h3.h3_to_geo_boundary(h3_index, geo_json=False)
         color = color_scale(time)
@@ -38,7 +40,6 @@ def app():
         fill_opacity=0.6,
         popup=f'Time: {time:.2f} min'
         ).add_to(map)
-
     st_folium(map, width=1400, height=700)
 
 if __name__ == "__main__":
